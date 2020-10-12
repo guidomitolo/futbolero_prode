3b4293bd48ace3375228167dc47e146fbd03f658
guidomitolo <guidomitolo@gmail.com>
2020-10-12 13:51:23 -0300

    doc_remains

--- test.py
+++
@@ -1,6 +0,0 @@
-from app import db
-from app.models import Points, User, Bets
-from app.helpers import up_rounds, fixture
-from sqlalchemy import desc, func
-from datetime import datetime
-import pandas as pd
